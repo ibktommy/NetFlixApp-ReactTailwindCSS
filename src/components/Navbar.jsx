8d74159cda488f24ab5b0cffafc4d86e8a5011ea
ibktommy <tomquartz@gmail.com>
2022-05-22 21:43:40 +0100

    Project files and dependencies setup and creation of Navbar Component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+	return (
+		<div className="w-full flex items-center justify-between p-4 z-[100] absolute">
+			<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+			<div>
+				<button className="text-white pr-4">Log In</button>
+				<button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Register</button>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
