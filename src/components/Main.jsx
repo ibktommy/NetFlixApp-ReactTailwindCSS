1c094ac516ae5b953aa9fd046f3f575ef0f45475
ibktommy <tomquartz@gmail.com>
2022-05-22 22:51:52 +0100

    Setting up call to our API server using the useEffect Hook and saving the data in our app state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import requests from "../Requests";
 
 const Main = () => {
 	const [movies, setMovies] = useState([]);
+
+	// Making API call using useEffect Hook whenever Component Did Mount
+	useEffect(() => {
+		axios.get(requests.requestPopular).then((response) => {
+			setMovies(response.data);
+		});
+	}, []);
+	console.log(movies);
+
 	return <div>Main</div>;
 };
 
