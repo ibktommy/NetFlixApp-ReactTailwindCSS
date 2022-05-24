b61d9f42e2189a7f36476773cfbea3b2759730cc
ibktommy <atomquartz@gmail.com>
2022-05-24 10:49:22 +0100

    Structuring the Main component elements and Styling it

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -18,6 +18,7 @@ const Main = () => {
 
 	// Get A Random Movie Data Object From our Request
 	const randomMovie = movies[Math.floor(Math.random() * movies.length)];
+	console.log(movies);
 
 	//Condition Statement to check if "movie" state has been updated with a valid object
 	if (randomMovie === undefined) {
@@ -33,6 +34,19 @@ const Main = () => {
 					src={`https://image.tmdb.org/t/p/original${randomMovie.backdrop_path}`}
 					alt={randomMovie.title}
 				/>
+				<div className="absolute w-full top-[20%] p-4 md:p-8">
+					<h1 className="text-3xl md:text-5xl font-bold">{randomMovie.title}</h1>
+					<div className="my-4">
+						<button className="border bg-gray-300 text-black border-gray-300 py-2 px-5">
+							Play Movie
+						</button>
+						<button className="border  text-white border-gray-300 py-2 px-5 ml-4">Watch Later</button>
+					</div>
+					<p className="text-gray-400 text-sm mt-2 mb-4">Released: {randomMovie.release_date}</p>
+					<p className="w-full md:max-w-[700%] lg:max-w-[50%] xl:max-w-w[35%] text-gray-200">
+						{randomMovie.overview}
+					</p>
+				</div>
 			</div>
 		</div>
 	);
