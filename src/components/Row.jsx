91447a3de6e1440e28c1c8d62f5069726f28de3f
ibktommy <atomquartz@gmail.com>
2022-05-24 16:08:02 +0100

    Adding Right and Left Arrow

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const Row = ({ title, fetchURL }) => {
 	const [rowMovie, setRowMovie] = useState([]);
@@ -28,12 +29,23 @@ const Row = ({ title, fetchURL }) => {
 			<h2 className="text-white font-normal uppercase tracking-wider text-center text-3xl md:text-4xl lg:text-5xl p-6 underline">
 				{title}
 			</h2>
-			<div className="relative text-white flex items-center">
-				<div>
+			<div className="relative text-white flex items-center group">
+				<MdChevronLeft
+					className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+					size={30}
+				/>
+				<div
+					id={"slider"}
+					className="relative w-full h-full whitespace-nowrap overflow-x-scroll scroll-smooth scrollbar-hide"
+				>
 					{rowMovie.map((item, id) => (
 						<Movie key={id} item={item} />
 					))}
 				</div>
+				<MdChevronRight
+					className="absolute bg-white right-0 rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+					size={30}
+				/>
 			</div>
 		</React.Fragment>
 	);
