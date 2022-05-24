654c9cec6c25db9cb3d222771279e5023ef27c43
ibktommy <atomquartz@gmail.com>
2022-05-24 13:18:10 +0100

    Creating the structure and styling to display movies on the webpage

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -17,12 +17,35 @@ const Row = ({ title, fetchURL }) => {
 			});
 	}, [fetchURL]);
 
-	console.log(rowMovie);
+	//Condition Statement to check if "movie" state has been updated with a valid object
+	if (rowMovie === undefined) {
+		return <div></div>;
+	}
+
 	return (
 		<React.Fragment>
 			<div>
-				<h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-				<div className="relative text-white flex items-center"></div>
+				<h2 className="text-white font-normal uppercase tracking-wider text-center text-3xl md:text-4xl lg:text-5xl p-6 underline">
+					{title}
+				</h2>
+				<div className="relative text-white flex items-center">
+					<div>
+						{rowMovie.map((item, id) => (
+							<div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+								<img
+									className="w-full h-auto block"
+									src={`https://image.tmdb.org/t/p/w500/${item.backdrop_path}`}
+									alt={item.title}
+								/>
+								<div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white transition-all">
+									<p className="text-white whitespace-normal text-xs md:text-sm font-medium flex justify-center items-center h-full text-center">
+										{item.title}
+									</p>
+								</div>
+							</div>
+						))}
+					</div>
+				</div>
 			</div>
 		</React.Fragment>
 	);
