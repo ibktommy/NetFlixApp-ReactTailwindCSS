a977d3c183b8801a5e5f277501473ff650eba6db
ibktommy <atomquartz@gmail.com>
2022-05-24 18:00:44 +0100

    Making the Arrow Icons Work for Sliding the images when the icons are clicked

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import Movie from "./Movie";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
 	const [rowMovie, setRowMovie] = useState([]);
 
 	// UseEffect Hook to run our API request when Component mounts or Updates
@@ -24,6 +24,16 @@ const Row = ({ title, fetchURL }) => {
 		return <div></div>;
 	}
 
+	// Function to Make The Arrow Icons Work - Slide the images when the icons are clicked
+	const sliderLeft = () => {
+		let slider = document.getElementById("slider" + rowID);
+		slider.scrollLeft = slider.scrollLeft - 300;
+	};
+	const sliderRight = () => {
+		let slider = document.getElementById("slider" + rowID);
+		slider.scrollLeft = slider.scrollLeft + 300;
+	};
+
 	return (
 		<React.Fragment>
 			<h2 className="text-white font-normal uppercase tracking-wider text-center text-3xl md:text-4xl lg:text-5xl p-6 underline">
@@ -31,11 +41,12 @@ const Row = ({ title, fetchURL }) => {
 			</h2>
 			<div className="relative text-white flex items-center group">
 				<MdChevronLeft
+					onClick={sliderRight}
 					className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
 					size={30}
 				/>
 				<div
-					id={"slider"}
+					id={"slider" + rowID}
 					className="relative w-full h-full whitespace-nowrap overflow-x-scroll scroll-smooth scrollbar-hide"
 				>
 					{rowMovie.map((item, id) => (
@@ -43,6 +54,7 @@ const Row = ({ title, fetchURL }) => {
 					))}
 				</div>
 				<MdChevronRight
+					onClick={sliderLeft}
 					className="absolute bg-white right-0 rounded-full opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
 					size={30}
 				/>
