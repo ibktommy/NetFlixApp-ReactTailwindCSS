98a5300b3711ffc5c837598e7da37d3b0cb64277
ibktommy <atomquartz@gmail.com>
2022-05-24 12:00:05 +0100

    Fetching movie Data in the row Component

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Row = ({ title, fetchURL }) => {
+	const [rowMovie, setRowMovie] = useState([]);
+
+	// UseEffect Hook to run our API request when Component mounts or Updates
+	useEffect(() => {
+		axios
+			.get(fetchURL)
+			.then((response) => {
+				setRowMovie(response.data.results);
+			})
+			.catch((error) => {
+				console.log(error.message);
+			});
+	}, [fetchURL]);
+
+	console.log(rowMovie);
 	return (
 		<React.Fragment>
 			<div>
 				<h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+				<div className="relative text-white flex items-center"></div>
 			</div>
 		</React.Fragment>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,14 @@ import Row from "../components/Row";
 import requests from "../Requests";
 
 const Home = () => {
-	const url = requests;
 	return (
 		<div>
 			<Main />
-			<Row title="Top Rated Movies" fetchURL={url.requestTopRated} />
-			<Row title="Trending Movies" fetchURL={url.requestTrending} />
-			<Row title="Horror Movies" fetchURL={url.requestHorror} />
-			<Row title="Upcoming Movies" fetchURL={url.requestUpcoming} />
-			<Row title="Popular Movies" fetchURL={url.requestPopular} />
+			<Row title="Top Rated Movies" fetchURL={requests.requestTopRated} />
+			<Row title="Trending Movies" fetchURL={requests.requestTrending} />
+			<Row title="Horror Movies" fetchURL={requests.requestHorror} />
+			<Row title="Upcoming Movies" fetchURL={requests.requestUpcoming} />
+			<Row title="Popular Movies" fetchURL={requests.requestPopular} />
 		</div>
 	);
 };
