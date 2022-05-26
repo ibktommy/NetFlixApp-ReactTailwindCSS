c4dfe75af1af77f6969a07f493002f9a9d9cfff7
ibktommy <atomquartz@gmail.com>
2022-05-26 18:39:59 +0100

    Creating Our Link-Pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
 	return (
@@ -11,6 +14,9 @@ function App() {
 				<Navbar />
 				<Routes>
 					<Route exact path="/" element={<Home />}></Route>
+					<Route exact path="/account" element={<Account />}></Route>
+					<Route exact path="/login" element={<Login />}></Route>
+					<Route exact path="/register" element={<Register />}></Route>
 				</Routes>
 			</AuthContextProvider>
 		</React.Fragment>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	return (
 		<div className="w-full flex items-center justify-between p-4 z-[100] absolute">
-			<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+			<Link to="/">
+				<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
+			</Link>
 			<div>
-				<button className="text-white pr-4">Log In</button>
-				<button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Register</button>
+				<Link to="/login">
+					<button className="text-white pr-4">Log In</button>
+				</Link>
+				<Link to="/register">
+					<button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Register</button>
+				</Link>
 			</div>
 		</div>
 	);
