de97a04f94f6e267f689431c840699d79a7ef355
ibktommy <atomquartz@gmail.com>
2022-05-26 19:03:20 +0100

    Import image locally for register oage

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import homeBgImage from "../image/home-bg.jpeg";
 
 const Register = () => {
-	return <div>Register</div>;
+	return (
+		<React.Fragment>
+			<div className="w-full h-screen">
+				<img src={homeBgImage} alt="home-bg" className="absolute w-full h-full object-cover" />
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Register;
