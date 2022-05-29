812afa2291d87a3b59ae0149605aad83936e076e
ibktommy <atomquartz@gmail.com>
2022-05-29 16:43:12 +0100

    Function that handles the actions that take place when we click the LogOut button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,31 @@ import { UserAuth } from "../context/AuthContext";
 
 const Navbar = () => {
 	const { user, logOut } = UserAuth();
-	console.log(user);
+
+	// Function that handles the actions that take place when we click the LogOut button
+	const handleLogOut = async () => {
+		try {
+			await logOut();
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
+
 	return (
 		<div className="w-full flex items-center justify-between p-4 z-[100] absolute">
 			<Link to="/">
 				<h1 className="text-red-600 text-4xl font-bold cursor-pointer">NETFLIX</h1>
 			</Link>
+			{/* Condition to Check if user object is valid in userAuth - if user is logged in */}
 			{user.email ? (
 				<div>
 					<Link to="/login">
 						<button className="text-white pr-4">Account</button>
 					</Link>
 					<Link to="/register">
-						<button className="bg-red-600 px-6 py-2 rounded cursor-pointer">Log Out</button>
+						<button onClick={handleLogOut} className="bg-red-600 px-6 py-2 rounded cursor-pointer">
+							Log Out
+						</button>
 					</Link>
 				</div>
 			) : (
