77f5dc86e10bf0ea6b881fbf7bdf86d5f1ddbb2a
ibktommy <atomquartz@gmail.com>
2022-05-30 11:05:57 +0100

    Structuring and Styling of the Account Page

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import homeBgImage from "../image/home-bg.jpeg";
 
 const Account = () => {
-	return <div>Account</div>;
+	return (
+		<React.Fragment>
+			<div className="w-full text-white">
+				<img className="w-full h-[400px] object-cover" src={homeBgImage} alt="home-bg" />
+				<div className="bg-black/60 fixed top-0 left-0 w-full h-[550px]"></div>
+				<div className="absolute top-[20%] p-4 md:p-8">
+					<h1 className="text-3xl md:text-5xl font-bold tracking-wide">My Shows</h1>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 };
 
 export default Account;
