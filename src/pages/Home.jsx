1dd8963eb1f8d4110b79cda71d6c6aea8ab255b7
ibktommy <atomquartz@gmail.com>
2022-09-04 22:04:47 +0100

    Rearrange the row to make 'Popular movies-section on top'

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,11 +7,27 @@ const Home = () => {
 	return (
 		<div>
 			<Main />
-			<Row rowID="5" title="Popular Movies" fetchURL={requests.requestPopular} />
-			<Row rowID="1" title="Top Rated Movies" fetchURL={requests.requestTopRated} />
-			<Row rowID="2" title="Trending Movies" fetchURL={requests.requestTrending} />
+			<Row
+				rowID="5"
+				title="Popular Movies"
+				fetchURL={requests.requestPopular}
+			/>
+			<Row
+				rowID="1"
+				title="Top Rated Movies"
+				fetchURL={requests.requestTopRated}
+			/>
+			<Row
+				rowID="2"
+				title="Trending Movies"
+				fetchURL={requests.requestTrending}
+			/>
 			<Row rowID="3" title="Horror Movies" fetchURL={requests.requestHorror} />
-			<Row rowID="4" title="Upcoming Movies" fetchURL={requests.requestUpcoming} />
+			<Row
+				rowID="4"
+				title="Upcoming Movies"
+				fetchURL={requests.requestUpcoming}
+			/>
 		</div>
 	);
 };
